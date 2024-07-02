5cb664c70daea8ab8a0c7b86967499a9be4c23e1
Christopher Sampson <Christopher.Sampson@SomersetFT.nhs.uk>
2024-07-02 07:30:03 +0100

    Potential fix for ending with a capacity node?

--- sfttoolbox/DES/_des.py
+++ sfttoolbox/DES/_des.py
@@ -270,7 +270,7 @@ class Simulation:
 
         logger.info(f"Moving to next node: {next_node}")
 
-        if self.graph.out_degree[next_node] > 0:
+        if self.graph.out_degree[next_node] > 0 and next_node not in self.capacities.keys():
             return self.traverse_graph(next_node, patient)
         else:
             # Final node reached
