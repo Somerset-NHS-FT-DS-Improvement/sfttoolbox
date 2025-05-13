e744b01c95a57b3716a81d24bd83e901d70731b2
Sam Vautier <106234937+sam-vautier@users.noreply.github.com>
2025-05-13 12:29:13 +0100

    add new team colour - orange (#27)
    * add new team colour - orange
    
    * __init__.py file - format code with black

--- sfttoolbox/__init__.py
+++ sfttoolbox/__init__.py
@@ -8,5 +8,6 @@ team_colours = {
     "grey": {"hex": "#dde6ed", "rgb": "RGB(221, 230, 237)"},
     "blue": {"hex": "#00789c", "rgb": "RGB(0, 120, 156)"},
     "dark_blue": {"hex": "#27374d", "rgb": "RGB(39, 55, 77)"},
+    "orange": {"hex": "#eb7956", "rgb": "RGB(235, 121, 86)"},
     "white": {"hex": "#ffffff", "rgb": "RGB(255, 255, 255)"},
 }
